5bcdbba1e9b83e5c46a7e65ab0337a4f6f282f82
Diogo-Gomes <37155647+Diogo-Gomes@users.noreply.github.com>
2018-11-30 12:16:31 -0200

    Update and rename ler_criar_certo.c to ler_criar.c

--- 2_PCs/codigos/ler_criar.c
+++ 2_PCs/codigos/ler_criar.c
@@ -9,10 +9,15 @@ int main(void)
 {
 
 FILE    *infile_nome;
+FILE    *infile_cpf;
+
 char    *nome_buffer;//buffer
-char	nome[51];
+char	nome[60];
 char	cpf[22];
 char	nome_txt[51];
+char	rg[14];
+char	data[35];
+char	pais[120];
 char	k;
 int 	fp, i;
 long    numbytes;
@@ -20,8 +25,8 @@ long    numbytes;
 // abre um arquivo existente para leitura
 infile_nome = fopen("nome.txt", "r");
  
-if(infile_nome == NULL)
-    return 1;
+if(infile_nome == NULL) {
+    return 1; }
  
 // Obtém o número de bytes 
 fseek(infile_nome, 0L, SEEK_END);
@@ -34,29 +39,42 @@ fseek(infile_nome, 0L, SEEK_SET);
 nome_buffer = (char*)calloc(numbytes, sizeof(char));	
  
 // erro de memoria
-if(nome_buffer == NULL)
-    return 1;
+if(nome_buffer == NULL) {
+    return 1; }
  
 // copiar todo o texto para o buffer
 fread(nome_buffer, sizeof(char), numbytes, infile_nome);
 fclose(infile_nome);
 
-int len_nome, len_cpf;
-
-
+int len_nome, len_cpf, len_rg, len_data, len_pais;
 
 strcpy(nome, &nome_buffer[0]);
 strcpy(nome_txt, &nome_buffer[0]); 
 strcat(nome_txt,".txt");
-printf("%s\n", nome);
+//printf("%s\n", nome);
 
 strcpy(cpf,"CPF: ");
 strcat(cpf,"123.456.789-99");
 strcat(cpf,"\n");
 
-len_cpf = strlen(cpf);
-len_nome = strlen(nome_txt);
+strcpy(rg,"RG: ");
+strcat(rg,"1.234.567");
+strcat(rg,"\n");
 
+strcpy(data,"DATA DE NASCIMENTO: ");
+strcat(data,"01/10/1010");
+strcat(data,"\n");
+
+strcpy(pais,"NOME DOS PAIS: ");
+strcat(pais,"MÃE DO FULANO \n PAI DO FULANO");
+strcat(pais,"\n");
+
+
+len_nome = strlen(nome_txt);
+len_cpf = strlen(cpf);
+len_rg = strlen(rg);
+len_data = strlen(data);
+len_pais = strlen(pais);
 
 //printf("tamanho = %d\n", len_nome);
 
@@ -78,11 +96,11 @@ for(i=0; nome[i]; i++) {
 		}
 	}
 write(fp, cpf, len_cpf);
+write(fp, rg, len_rg);
+write(fp, data, len_data);
+write(fp, pais, len_pais);
+
 close(fp);
-
-
-
-
 
 return 0;
 }
